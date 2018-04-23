14713942c55264d76c43c68e42475b2f4a907d7b
Ramadan Ahmed Ramadan <ramadan.ahmedr93@gmail.com>
2018-04-23 16:18:57 +0200

    Add Majority problem solution

--- Algorithmic Toolbox/Week4/majority_element.cpp
+++ Algorithmic Toolbox/Week4/majority_element.cpp
@@ -8,7 +8,22 @@ int get_majority_element(vector<int> &a, int left, int right) {
     if (left == right) return -1;
     if (left + 1 == right) return a[left];
     //write your code here
-    return -1;
+    std::sort(a.begin(), a.end());
+    int current_lookup = -1, lookup_counter = 0;
+    int found = -1;
+    for (int i : a) {
+        if (current_lookup != i) {
+            current_lookup = i;
+            lookup_counter = 1;
+        } else lookup_counter++;
+
+        if (lookup_counter > (a.size() / 2)) {
+            found = 1;
+            break;
+        }
+    }
+
+    return found;
 }
 
 int main() {
