21615b73546b46d766225de32d24aeeeb39330ad
Ramadan Ahmed Ramadan <ramadan.ahmedr93@gmail.com>
2018-05-10 01:01:15 +0200

    Add Solution to 3-way partition Quick sort

--- Algorithmic Toolbox/Week4/sorting.cpp
+++ Algorithmic Toolbox/Week4/sorting.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <cassert>
 
 using std::vector;
 using std::swap;
@@ -18,6 +19,24 @@ int partition2(vector<int> &a, int l, int r) {
     return j;
 }
 
+// add 3-way partition to solve quick sort problem
+void partition3(vector<int> &a, int low, int high, int &midLow, int &midHigh) {
+    int pivot = a[low];
+    midLow = low, midHigh = high;
+    int i = low + 1;
+    while (i <= midHigh) {
+        // every thing larger than the pivot goes right
+        if (pivot < a[i])
+            swap(a[i], a[midHigh--]);
+            // every thing smaller than the pivot goes left
+        else if (pivot > a[i])
+            swap(a[i++], a[midLow++]);
+
+        else i++;
+    }
+
+}
+
 void randomized_quick_sort(vector<int> &a, int l, int r) {
     if (l >= r) {
         return;
@@ -25,21 +44,37 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
 
     int k = l + rand() % (r - l + 1);
     swap(a[l], a[k]);
-    int m = partition2(a, l, r);
+    int midLow, midHigh;
+    partition3(a, l, r, midLow, midHigh);
 
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    randomized_quick_sort(a, l, midLow - 1);
+    randomized_quick_sort(a, midHigh + 1, r);
+}
+
+void testSolution() {
+
+    vector<int> vec = {2, 3, 9, 2, 2};
+    vector<int> ans = {2, 2, 2, 3, 9};
+    randomized_quick_sort(vec, 0, (vec.size() - 1));
+    for (int i : vec) {
+        std::cout << i << ' ';
+    }
+    // check if array is sorted
+    for (int i = 0; i < vec.size(); i++)
+        assert(vec[i] == ans[i]);
+
 }
 
 int main() {
     int n;
     std::cin >> n;
     vector<int> a(n);
-    for (size_t i = 0; i < a.size(); ++i) {
-        std::cin >> a[i];
+    for (int &i : a) {
+        std::cin >> i;
     }
-    randomized_quick_sort(a, 0, a.size() - 1);
-    for (size_t i = 0; i < a.size(); ++i) {
-        std::cout << a[i] << ' ';
+    randomized_quick_sort(a, 0, static_cast<int>(a.size() - 1));
+    for (int i : a) {
+        std::cout << i << ' ';
     }
+//    testSolution();
 }
