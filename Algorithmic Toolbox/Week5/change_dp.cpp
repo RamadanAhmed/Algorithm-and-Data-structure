3464a1d1eca75a5780e79080c64371c811a156c1
RamadanAhmed <ramadan.ahmedr93@gmail.com>
2018-05-13 20:55:55 +0200

    Add Solution to Change problem

--- Algorithmic Toolbox/Week5/change_dp.cpp
+++ Algorithmic Toolbox/Week5/change_dp.cpp
@@ -1,12 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <climits>
+#include <cassert>
 
-int get_change(int m) {
-  //write your code here
-  return m / 4;
+using std::vector;
+
+int get_change(int money, vector<int> &MinCoins, const vector<int> &Denominations) {
+    MinCoins[0] = 0;
+    for (int i = 1; i <= money; ++i) {
+        for (auto coin : Denominations) {
+            if (i >= coin) {
+                int NumCoins = MinCoins[i - coin] + 1;
+                if (NumCoins < MinCoins[i]) {
+                    MinCoins[i] = NumCoins;
+                }
+            }
+        }
+    }
+    return MinCoins[money];
+}
+
+void TestSolution() {
+    vector<int> g_Denominations = {1, 3, 4};
+    {
+        vector<int> g_MinNumCoins(2 + 1, INT_MAX);
+        assert(get_change(2, g_MinNumCoins, g_Denominations) == 2);
+    }
+    {
+        vector<int> g_MinNumCoins(34 + 1, INT_MAX);
+        assert(get_change(34, g_MinNumCoins, g_Denominations) == 9);
+    }
+    {
+        vector<int> g_MinNumCoins(6 + 1, INT_MAX);
+        assert(get_change(6, g_MinNumCoins, g_Denominations) == 2);
+    }
 }
 
 int main() {
-  int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+    // coin denomination is 1, 3, 4
+    vector<int> g_Denominations = {1, 3, 4};
+    int m;
+    std::cin >> m;
+    vector<int> g_MinNumCoins(m + 1,INT_MAX);
+    std::cout << get_change(m, g_MinNumCoins, g_Denominations) << '\n';
+    // TestSolution();
 }
