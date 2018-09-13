ab70d5715343ed183242158a51c58092806be5f6
Ramadan Ahmed <ramadanahmedramadan93@yahoo.com>
2018-09-13 13:22:15 +0200

    Add: Solve 0/1 knapsack problem

--- Algorithmic Toolbox/Week6/knapsack.cpp
+++ Algorithmic Toolbox/Week6/knapsack.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
@@ -15,15 +16,29 @@ int optimal_weight(int W, const vector<int> &w) {
 }
 
 int optimal_weight_dp(int W, const vector<int> &w) {
-    return 0;
+    std::vector<int> init(W + 1, 0);
+    std::vector<std::vector<int>> values(w.size(), init);
+    for (int i = 1; i < W + 1; i++) {
+        for (int j = 1; j < w.size(); j++) {
+            values[j][i] = values[j - 1][i];
+            if (w[j - 1] <= i) {
+                int candidate = w[j - 1] + values[j - 1][i - w[j - 1]];
+                if (candidate > values[j][i]) {
+                    values[j][i] = candidate;
+                }
+            }
+        }
+    }
+    return values[w.size()][W];
 }
 
 int main() {
-    int n, W;
+    int W;
+    unsigned long n;
     std::cin >> W >> n;
     vector<int> w(n);
     for (int i = 0; i < n; i++) {
         std::cin >> w[i];
     }
-    std::cout << optimal_weight(W, w) << '\n';
+    std::cout << optimal_weight_dp(W, w) << '\n';
 }
